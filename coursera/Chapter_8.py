9eab3269e46ab73290fb74800555519287704e14
dark4igi <dark4igi@gmail.com>
2020-04-16 01:15:46 +0300

    1

--- coursera/Chapter_8.py
+++ coursera/Chapter_8.py
@@ -3,6 +3,7 @@ x = ['123', ['1', 'dsa'], 123, True, 23.65]
 print (x)
 print (type(x))
 print (len(x))
+print(dir(x))
 print ()
 for y in x:
     print (y)
@@ -19,3 +20,25 @@ z = [1, 2, 3, 4, 5]
 print (z)
 z[2] = 9
 print (z)
+print()
+
+a = [1, 2, 3]
+b = [4, 5, 6]
+c = a + b
+print (c)
+print (max(c))
+print(min(c))
+print(sum(c))
+print(sum(c) / len (c))
+print ()
+
+d = list()
+print (d)
+d.append('book')
+d.append(99)
+print (d)
+d.append(32.53)
+print(d)
+
+print( 99 in d)
+print ('dsa' in d)
